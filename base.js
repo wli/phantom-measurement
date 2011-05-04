7644449eb7bfe5ac80309e1ce4984bf75c7662de
Richard Shin <ricshin@berkeley.edu>
2011-05-04 16:31:13 -0700

    Save PhantomJS load time.

--- base.js
+++ base.js
@@ -26,6 +26,8 @@
       var results = {"failed_to_load": true};
     }
 
+    results['load_time'] = phantom.loadTime / 1000; // Milliseconds to seconds
+
     phantom.setOutputPath(phantom.args[1]);
     phantom.write(JSON.stringify(results));
     phantom.exit();
